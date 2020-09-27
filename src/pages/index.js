13ae333de3ff14b31ba146b3af89399677502db4
Aaron Chan <aaron.y.chan64@gmail.com>
2020-09-26 19:11:54 -0600

    Add tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,6 +47,19 @@ const IndexPage = () => (
     <br />
     <h4>Error banner</h4>
     <ErrorBanner title="title" msg="messge" visible={true}/>
+
+    <h4>Tags</h4>
+    <div className="px-6 pt-4 pb-2">
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        #photography
+      </span>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        #travel
+      </span>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        #winter
+      </span>
+    </div>
   </Layout>
 )
 
